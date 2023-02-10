db6da833c46a031e98ff6ed8ae0a04e691dae8c0
DJJo <ssshooterx@gmail.com>
2023-02-10 13:53:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,14 @@ await fastify.register(cors, {
 // https://media.kitsu.io/characters/images/3/original.jpg
 await fastify.register(proxy, {
   upstream: 'https://media.kitsu.io',
-  prefix: '/api', // optional
+  prefix: '/kitsu', // optional
   http2: false, // optional
+})
+
+// https://s4.anilist.co/file/anilistcdn/character/medium/b284158-z1uGKC3IYVa1.png
+await fastify.register(proxy, {
+  upstream: 'https://s4.anilist.co',
+  prefix: '/anilist',
 })
 
 await fastify.listen({ port: process.env.PORT || 2333, address: '0.0.0.0' })
